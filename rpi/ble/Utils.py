14afba150fc1ae7539b2c3ce983be3f81808e45a
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-05-08 10:31:33 -0700

    added ble-server classes

---
+++ rpi/ble/Utils.py
@@ -0,0 +1,16 @@
+import dbus
+
+
+import dbus
+from Constants import *
+
+def find_adapter(bus : dbus, adapter: str):
+    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
+                               DBUS_OM_IFACE)
+    objects = remote_om.GetManagedObjects()
+
+    for o, props in objects.items():
+        if adapter in props.keys():
+            return o
+
+    return None
